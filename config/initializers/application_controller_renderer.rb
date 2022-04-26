4c7ce7ca3e9625aa89a6a8164e631315cf471bde
hacchiko <valkyria.zer0256@gmail.com>
2022-04-27 00:02:37 +0900

    add 詳細ページ実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5c9d82ebb050f4112fd8f7fc6b955f88cfe342a0a1016ba7b0fb803d264b433471d569711b80941ed2682bcabccaae4e2dc8b116118a8866f667045846ada76c'
